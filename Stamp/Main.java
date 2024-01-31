6ecee9d22f37fdee46159b313f4b2da50701ed86
alicezo0307 <151118219+alicezo0307@users.noreply.github.com>
2024-01-31 08:35:33 +0700

    Update Main.java

--- Stamp/Main.java
+++ Stamp/Main.java
@@ -1,1 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab4;
+
+
+public class Lab4 {
+
+    public static void main(String[] args) {
+        /*
+        System.out.println("Hello World");
+        Stamp st = new Stamp();
+        System.out.println(st.numOfStamp);
+        st.stamp();
+        st.stamp();
+        System.out.println(st.numOfStamp);
+        st.maxRefill();
+        st.stamp();
+        System.out.println(st.numOfStamp);
+*/
+    }
+    
+}
 
