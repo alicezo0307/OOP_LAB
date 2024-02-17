e61ebbb4965c5ccc35305c70a8cbbb361b37b593
alicezo0307 <151118219+alicezo0307@users.noreply.github.com>
2024-02-17 23:08:52 +0700

    Update figure.java

--- BoardGanme/figure.java
+++ BoardGanme/figure.java
@@ -1,19 +1,31 @@
-public class Figure{
+public class Figure {
+
     String nameFigure;
     int positionFigure;
     
-    public Figure(String name,int position) {
-        this.nameFigure = name;
-        this.positionFigure=position;
+    void moveDestination() {
+        System.out.print("["+getName() +"] Position : "+ getPosition()+"\n");
+        
     }
-    
-    String getName(){
+    /*
+    void moveDestination(Board board){
+        for (int i = 0; i < 10; i++) {
+            System.out.print(board.grid[i]);
+        }
+    }*/
+
+    public Figure(String name, int position) {
+        this.nameFigure = name;
+        this.positionFigure = position;
+
+    }
+
+    String getName() {
         return this.nameFigure;
     }
-    
-    int getPosition(){
-        return this.positionFigure;
+
+    int getPosition() {
+        return this.positionFigure + 1;
     }
-    
-    
+
 }
